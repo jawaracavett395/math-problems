b42502840965e1243ddad2bac30a19c8d6cb153d
jawaracavett395 <jawaracavett395@gmail.com>
2025-03-06 15:18:24 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,15 @@
+int main() {
+    int x = 5;
+    int y = 7;
+    int sum = x + y;
+    int diff = x - y;
+    int prod = x * y;
+    int quot = x / y;
+    int rem = x % y;
+    printf("The sum is: %d\n", sum);
+    printf("The difference is: %d\n", diff);
+    printf("The product is: %d\n", prod);
+    printf("The quotient is: %d\n", quot);
+    printf("The remainder is: %d\n", rem);
+    return 0;
+}
