9d7cc6bf74e893a5709c76f933479d21e9be27d5
jawaracavett395 <jawaracavett395@gmail.com>
2025-03-16 11:03:03 +0000

    Add 11.c

---
+++ src/11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int a = 5;
+    int b = 7;
+    int c = 2;
+    int d = 9;
+    printf("The sum of %d and %d is %d\n", a, b, a + b);
+    printf("The product of %d and %d is %d\n", c, d, c * d);
+    return 0;
+}
