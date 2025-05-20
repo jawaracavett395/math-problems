74780d8f639a612bddee2b48aa3f1fc96d41cdb7
jawaracavett395 <jawaracavett395@gmail.com>
2025-05-20 02:59:25 +0000

    Add 40.c

---
+++ src/40.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    
+    if (num1 > num2) {
+        printf("num1 is greater than num2.\n");
+    } else {
+        printf("num1 is not greater than num2.\n");
+    }
+    
+    return 0;
+}
